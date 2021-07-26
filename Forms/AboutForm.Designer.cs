47da1aac2f822d04e3c8d93476d258d49635765a
AdotBdot <adam.buchowski1@gmail.com>
2021-07-26 12:15:30 +0200

    Added links

--- Forms/AboutForm.Designer.cs
+++ Forms/AboutForm.Designer.cs
@@ -38,6 +38,13 @@ namespace SpicetifyManager
             this.SpicetifyVer = new System.Windows.Forms.Label();
             this.SpotifyVer = new System.Windows.Forms.Label();
             this.Image = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.GithubLink = new System.Windows.Forms.LinkLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BugReportLink = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.IconsProviderLink = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
@@ -46,9 +53,9 @@ namespace SpicetifyManager
             this.label1.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(19, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 22);
+            this.label1.Size = new System.Drawing.Size(72, 22);
             this.label1.TabIndex = 41;
-            this.label1.Text = "Manager Version";
+            this.label1.Text = "Version:";
             // 
             // label2
             // 
@@ -56,9 +63,9 @@ namespace SpicetifyManager
             this.label2.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(19, 87);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 22);
+            this.label2.Size = new System.Drawing.Size(79, 22);
             this.label2.TabIndex = 42;
-            this.label2.Text = "Spicetify Version";
+            this.label2.Text = "Spicetify:";
             // 
             // label3
             // 
@@ -66,9 +73,9 @@ namespace SpicetifyManager
             this.label3.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(19, 113);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 22);
+            this.label3.Size = new System.Drawing.Size(68, 22);
             this.label3.TabIndex = 43;
-            this.label3.Text = "Spotify Version";
+            this.label3.Text = "Spotify:";
             // 
             // Title
             // 
@@ -87,7 +94,7 @@ namespace SpicetifyManager
             this.ManagerVer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ManagerVer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ManagerVer.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ManagerVer.Location = new System.Drawing.Point(208, 61);
+            this.ManagerVer.Location = new System.Drawing.Point(189, 61);
             this.ManagerVer.Name = "ManagerVer";
             this.ManagerVer.Size = new System.Drawing.Size(47, 24);
             this.ManagerVer.TabIndex = 45;
@@ -98,7 +105,7 @@ namespace SpicetifyManager
             this.SpicetifyVer.AutoSize = true;
             this.SpicetifyVer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.SpicetifyVer.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SpicetifyVer.Location = new System.Drawing.Point(208, 87);
+            this.SpicetifyVer.Location = new System.Drawing.Point(189, 87);
             this.SpicetifyVer.Name = "SpicetifyVer";
             this.SpicetifyVer.Size = new System.Drawing.Size(47, 24);
             this.SpicetifyVer.TabIndex = 46;
@@ -109,7 +116,7 @@ namespace SpicetifyManager
             this.SpotifyVer.AutoSize = true;
             this.SpotifyVer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.SpotifyVer.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SpotifyVer.Location = new System.Drawing.Point(208, 113);
+            this.SpotifyVer.Location = new System.Drawing.Point(189, 113);
             this.SpotifyVer.Name = "SpotifyVer";
             this.SpotifyVer.Size = new System.Drawing.Size(47, 24);
             this.SpotifyVer.TabIndex = 47;
@@ -124,11 +131,90 @@ namespace SpicetifyManager
             this.Image.Size = new System.Drawing.Size(80, 80);
             this.Image.TabIndex = 55;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 22);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "GitHub:";
+            // 
+            // GithubLink
+            // 
+            this.GithubLink.AutoSize = true;
+            this.GithubLink.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.GithubLink.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GithubLink.Location = new System.Drawing.Point(189, 165);
+            this.GithubLink.Name = "GithubLink";
+            this.GithubLink.Size = new System.Drawing.Size(348, 24);
+            this.GithubLink.TabIndex = 57;
+            this.GithubLink.TabStop = true;
+            this.GithubLink.Text = "github.com/AdotBdot/SpicetifyManagerApp";
+            this.GithubLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GithubLink_LinkClicked);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BugReportLink
+            // 
+            this.BugReportLink.AutoSize = true;
+            this.BugReportLink.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BugReportLink.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BugReportLink.Location = new System.Drawing.Point(189, 191);
+            this.BugReportLink.Name = "BugReportLink";
+            this.BugReportLink.Size = new System.Drawing.Size(401, 24);
+            this.BugReportLink.TabIndex = 59;
+            this.BugReportLink.TabStop = true;
+            this.BugReportLink.Text = "github.com/AdotBdot/SpicetifyManagerApp/issues";
+            this.BugReportLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.BugReportLink_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 22);
+            this.label5.TabIndex = 58;
+            this.label5.Text = "Bug Report:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 22);
+            this.label6.TabIndex = 60;
+            this.label6.Text = "Icons Provider:";
+            // 
+            // IconsProviderLink
+            // 
+            this.IconsProviderLink.AutoSize = true;
+            this.IconsProviderLink.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IconsProviderLink.Font = new System.Drawing.Font("Open Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IconsProviderLink.Location = new System.Drawing.Point(189, 245);
+            this.IconsProviderLink.Name = "IconsProviderLink";
+            this.IconsProviderLink.Size = new System.Drawing.Size(69, 24);
+            this.IconsProviderLink.TabIndex = 61;
+            this.IconsProviderLink.TabStop = true;
+            this.IconsProviderLink.Text = "Freepik";
+            this.IconsProviderLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.IconsProviderLink_LinkClicked);
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 461);
+            this.Controls.Add(this.IconsProviderLink);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BugReportLink);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.GithubLink);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.Image);
             this.Controls.Add(this.SpotifyVer);
             this.Controls.Add(this.SpicetifyVer);
@@ -157,5 +243,12 @@ namespace SpicetifyManager
         private System.Windows.Forms.Label SpicetifyVer;
         private System.Windows.Forms.Label SpotifyVer;
         private System.Windows.Forms.Panel Image;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel GithubLink;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.LinkLabel BugReportLink;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel IconsProviderLink;
     }
 }
--- Forms/AboutForm.cs
+++ Forms/AboutForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace SpicetifyManager
@@ -32,17 +33,44 @@ namespace SpicetifyManager
             label1.ForeColor = Colors.TxtLight;
             label2.ForeColor = Colors.TxtLight;
             label3.ForeColor = Colors.TxtLight;
+            label4.ForeColor = Colors.TxtLight;
+            label5.ForeColor = Colors.TxtLight;
+            label6.ForeColor = Colors.TxtLight;
             ManagerVer.ForeColor = Colors.TxtLight;
             ManagerVer.BackColor = Colors.GetBg(1);
             SpicetifyVer.ForeColor = Colors.TxtLight;
             SpicetifyVer.BackColor = Colors.GetBg(1);
             SpotifyVer.ForeColor = Colors.TxtLight;
             SpotifyVer.BackColor = Colors.GetBg(1);
+            GithubLink.LinkColor = Colors.Primary;
+            GithubLink.BackColor = Colors.GetBg(1);
+            BugReportLink.LinkColor = Colors.Primary;
+            BugReportLink.BackColor = Colors.GetBg(1);
+            IconsProviderLink.LinkColor = Colors.Primary;
+            IconsProviderLink.BackColor = Colors.GetBg(1);
         }
 
 
         private void AboutForm_Load(object sender, EventArgs e)
         {
+        }
+
+        private void GithubLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo SInfo = new ProcessStartInfo("https://github.com/AdotBdot/SpicetifyManagerApp");
+            Process.Start(SInfo);
+        }
+
+        private void BugReportLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo SInfo = new ProcessStartInfo("https://github.com/AdotBdot/SpicetifyManagerApp/issues");
+            Process.Start(SInfo);
+        }
+
+        private void IconsProviderLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo SInfo = new ProcessStartInfo("https://www.flaticon.com/");
+            Process.Start(SInfo);
         }
     }
 }
