3b64623dec03f94b12ceaf1a781987523bf4cece
AdotBdot <adam.buchowski1@gmail.com>
2021-07-29 10:40:20 +0200

    removed unused usings

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Text;
-using System.IO;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace SpicetifyManager
