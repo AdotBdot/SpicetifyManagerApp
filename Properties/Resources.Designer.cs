6bc235c08e45b48e806d7237dae4f834b5622eca
AdotBdot <adam.buchowski1@gmail.com>
2021-07-29 10:35:10 +0200

    naming

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SpicetifyManagerApp.Properties {
+namespace SpicetifyManager.Properties {
     using System;
     
     
