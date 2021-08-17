341d2276484cec94e38f403b0501a1053e7f61f3
AdotBdot <adam.buchowski1@gmail.com>
2021-08-17 20:35:31 +0200

    Added console toggle possibility

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Octokit;
 using Application = System.Windows.Forms.Application;
@@ -88,12 +89,23 @@ namespace SpicetifyManager
 
     internal static class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         private static void Main()
         {
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+
             My.Fonts.LoadFonts();
 
             string userDirectory = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\.spicetify\");
