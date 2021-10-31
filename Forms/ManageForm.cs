d84a8a6537568ba02f1d22572eda433a0ab7e2b3
AdotBdot <adam.buchowski1@gmail.com>
2021-10-31 11:25:25 +0100

    Localization

--- Forms/ManageForm.cs
+++ Forms/ManageForm.cs
@@ -102,27 +102,27 @@ namespace SpicetifyManager
 
         private void UpgradeBtn_MouseHover(object sender, EventArgs e)
         {
-            ToolTip.Show("Upgrade Spicetify", UpgradeBtn);
+            ToolTip.Show("Upgrade Spicetify.", UpgradeBtn);
         }
 
         private void RestartBtn_MouseHover(object sender, EventArgs e)
         {
-            ToolTip.Show("Restart Spotify", RestartBtn);
+            ToolTip.Show("Restart Spotify.", RestartBtn);
         }
 
         private void BackupBtn_MouseHover(object sender, EventArgs e)
         {
-            ToolTip.Show("Backup Spotify files", BackupBtn);
+            ToolTip.Show("Backup Spotify files.", BackupBtn);
         }
 
         private void ClearBackupBtn_MouseHover(object sender, EventArgs e)
         {
-            ToolTip.Show("Clear backup", ClearBackupBtn);
+            ToolTip.Show("Clear backup.", ClearBackupBtn);
         }
 
         private void RestoreBtn_MouseHover(object sender, EventArgs e)
         {
-            ToolTip.Show("Restores Spotify to it's original state", RestoreBtn);
+            ToolTip.Show("Restore Spotify to it's original state.", RestoreBtn);
         }
     }
 }
